e9f2277d4c21c130a75d28195639024a36f3a7a7
missyalienn <allan.missy@gmail.com>
2017-02-14 15:11:30 -0500

    fixed fourth label

--- CustomCellMath/TableViewController.swift
+++ CustomCellMath/TableViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     
     // an array of arrays
     // i.e. [[1, 2, 3, 4], [5, 3, 1, 0], [5, 2, 6, 6]]
@@ -20,9 +25,50 @@ class TableViewController: UITableViewController {
     }
     
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.numbers.count
+    }
     
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "mathCell", for: indexPath) as! MathTableViewCell
+        
+        
+       let arrayIndex = indexPath.row
+       var selectedArray = self.numbers[arrayIndex]
+        
+       cell.firstNumberLabel.text = String(selectedArray[0])
+        
+       cell.secondNumberLabel.text = String(selectedArray[1])
+        
+        cell.thirdNumberLabel.text = String(selectedArray[2])
+        
+        cell.fourthNumberLabel.text = String(selectedArray[3])
+
+        return cell
+    }
     
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "showMath" {
+            
+            let dest = segue.destination as! DisplayMathViewController
+            
+            let selectedNum = tableView.indexPathForSelectedRow?.row
+    
+                if let unwrappedRow = selectedNum {
+                    dest.numbers = self.numbers[unwrappedRow]
+        
+            }
+        
+            
+        }
+        
+        
+    }
+    
+    
 
 }
 
